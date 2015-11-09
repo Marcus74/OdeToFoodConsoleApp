891de00b8ddb01498b9db9ce38280f88a558f032
Mark Huff <huffma@gmail.com>
2015-11-08 21:57:41 -0500

    Fixed path to msbuild

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-set PATH=%PATH%;C:\Program Files (x86)\MSBuild\12.0\Bin
+set PATH=%PATH%;C:\Program Files (x86)\MSBuild\14.0\Bin
 msbuild OdeToFoodConsoleApp.sln /t:Clean
 msbuild OdeToFoodConsoleApp.sln /t:build /p:Configuration=Debug
